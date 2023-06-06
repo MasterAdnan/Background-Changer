e7035a543f3fad2ff8744e1d7f402307ff3d18ba
Adnan <charmingadnan17@gmail.com>
2023-06-05 22:23:41 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+
+let a=document.getElementById("bg1")
+let b=document.getElementById("bg")
+
+
+
+
+a.addEventListener('click',function(e){
+    document.body.style.backgroundColor="green" ;
+})
+
+b.addEventListener('click',function(f){
+    document.body.style.backgroundColor="blue" ;
+})
+
+
